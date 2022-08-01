7c6edb059f649f35d1994f721dfd14574cafac93
chen9 <chen94013@zohomail.cn>
2022-08-02 00:35:32 +0800

    删除注释

--- src/main/java/com/two/service/impl/BookServiceImpl.java
+++ src/main/java/com/two/service/impl/BookServiceImpl.java
@@ -29,33 +29,5 @@ public class BookServiceImpl extends ServiceImpl<BookDao, Book> implements IBook
         return bookDao.deleteById(id)>0;
     }
 
-   /* @Override
-    public IPage<Book> getPage(int currentPage,int pageSize) {
-        IPage<Book> page=new Page<Book>(currentPage,pageSize);
-        bookDao.selectPage(page,null);
-        return page;
-    }*/
-
-//    @Override
-    /*public IPage<Book> getPage(int currentPage, int pageSize, Book book) {
-        *//*
-         * 使用封装条件
-         * *//*
-        LambdaQueryWrapper<Book> lqw = new LambdaQueryWrapper<Book>();
-        return page;
-    }*/
-/*
-    *like(判断是否进行后续判断，判断范围，待判断值)
-    *按条件查询的时候用到的
-*/
-       /* lqw.like(Strings.isNotEmpty(book.getType()),Book::getType,book.getType());
-        lqw.like(Strings.isNotEmpty(book.getName()),Book::getName,book.getName());
-        lqw.like(Strings.isNotEmpty(book.getDescription()),Book::getDescription,book.getDescription());
-        IPage<Book> page=new Page<Book>(currentPage,pageSize);*/
-/*
-* 漏加lqw了
-* */
-     /*   bookDao.selectPage(page,lqw);
-       */
 
 }
